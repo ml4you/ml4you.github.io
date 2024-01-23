b21bf6d124473ac4ca7588393596f419be8bddea
Yassir Boulaamane <7014404+yboulaamane@users.noreply.github.com>
2024-01-23 23:07:06 +0100

    Update dark-mode-toggle.js

--- assets/js/dark-mode-toggle.js
+++ assets/js/dark-mode-toggle.js
@@ -1,13 +1,16 @@
+import { document, Element } from 'some-package';
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    document.body.classList.add('dark-mode');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    document.body.classList.remove('dark-mode');
-  }    
-}
+if (toggleSwitch instanceof Element) {
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      document.body.classList.add('dark-mode');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      document.body.classList.remove('dark-mode');
+    }    
+  }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+  toggleSwitch.addEventListener('change', switchTheme, false);
+}
